a87f315dbc1b1132229654d773f937b78e3c1c71
Borisov Ruslan <rdborisov99@gmail.com>
2023-06-03 14:51:21 +0300

    Final report

---
+++ database/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-bindfs",["= 1.1.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"da3d8de2ffbad8ae8691a36190412593e288dcef4418362761460aad6070cb8c","vagrant_version":"2.3.4"}
